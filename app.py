4a0c73f9c121604a7e4c6972a8ef0937f4f3fc0e
stachu1190 <stachu1190@gmail.com>
2022-01-03 01:54:24 +0100

    exceptions, status code and all parsers

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from psycopg2 import Error
 import signal
 import parsers
 import sys
-from queries import employee_query
+import queries
 
 def shutdown(signal_received, frame):
     cursor.close()
@@ -17,6 +17,7 @@ signal.signal(signal.SIGTERM, shutdown)
 signal.signal(signal.SIGINT, shutdown)
 
 app = Flask(__name__)
+#app.secret_key = "siemaeniuwpuscmnie"
 api = Api(app)
 
 try:
@@ -31,16 +32,56 @@ try:
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
 
+def print_psycopg2_exception(err):
+    err_type, err_obj, traceback = sys.exc_info()
+    line_num = traceback.tb_lineno
+    print ("\npsycopg2 ERROR:", err, "on line number:", line_num)
+    print ("psycopg2 traceback:", traceback, "-- type:", err_type)
+    print ("\nextensions.Diagnostics:", err.diag)
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+    return err.pgcode, err.pgerror
+
 class Employee(Resource):
     def post(self):
         parser = parsers.employee_parser()
         args = parser.parse_args()
-        query = employee_query(args)
+        query = queries.employee_query(args)
         cursor.execute(query)
         connection.commit()
+        return {}, 201
+class Attendance(Resource):
+    def post(self):
+        parser = parsers.attendance_parser()
+        args = parser.parse_args()
+        query = queries.attendance_query(args)
+        cursor.execute("SELECT id_employee FROM employee WHERE id_employee = {id_employee}".format(id_employee = args['id_employee']))
+        if(cursor.fetchall() == []):
+            return {'error' : 'There is no employee with given id'},404
+        else:
+            try:
+                cursor.execute(query)
+                connection.commit()
+            except Exception as err:
+                code, error = print_psycopg2_exception(err)
+                connection.rollback()
+                if(code == "23505"):
+                    return {"error" : "Unique Violation - given record already exists"}, 409
+                else:
+                    return {"error" : error, "code" : code}, 400
+            return {}, 201
+class Contractor(Resource):
+    def post(self):
+        parser = parsers.contractor_parser()
+        args = parser.parse_args()
+        query = queries.contractor_query(args)
+        cursor.execute(query)
+        connection.commit()
+        return {}, 201
         
 
 api.add_resource(Employee, "/employee")
+api.add_resource(Attendance, "/attendance")
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- parsers.py
+++ parsers.py
@@ -1,9 +1,10 @@
 from flask_restful import reqparse
+
 def employee_parser():
     parser = reqparse.RequestParser()
     parser.add_argument("first_name", type=str, help="First name of the employee", required=True)
     parser.add_argument("last_name", type=str, help="Last name of the employee", required=True)
-    parser.add_argument("PESEL", type=str, help="Numer PESEL pracownika", required=True)
+    parser.add_argument("PESEL", type=str, help="Employee PESEL", required=True)
     parser.add_argument("date_employed", type=str, help="Date in format YYYY-MM-DD", required=True)
     parser.add_argument("salary", type=float, help="Employee salary as float", required=True)
     parser.add_argument("city", type=str, help="City name", required=True)
@@ -12,6 +13,77 @@ def employee_parser():
     parser.add_argument("country", type=str, help="Country name", required=True)
     parser.add_argument("house_number", type=int, help="House number", required=True)
     return parser
+def attendance_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("id_employee", type=int, help="ID of the employee", required=True)
+    parser.add_argument("attendance_date", type=str, help="Date in format YYYY-MM-DD", required=True)
+    parser.add_argument("time_at_work", type=int, help="hours in work passed as an integer", required=True)
+    return parser
+def contractor_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("name_company", type=str, help="Name of the company", required=True)
+    parser.add_argument("nip", type=str, help="NIP of the company", required=True)
+    parser.add_argument("email", type=str, help="Company's email address", required=True)
+    parser.add_argument("city", type=str, help="City name", required=True)
+    parser.add_argument("street", type=str, help="Street name", required=True)
+    parser.add_argument("postal_code", type=str, help="City postal code", required=True)
+    parser.add_argument("country", type=str, help="Country name", required=True)
+    parser.add_argument("house_number", type=int, help="House number", required=True)
+    return parser
+def supplier_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("name_company", type=str, help="Name of the company", required=True)
+    parser.add_argument("nip", type=str, help="NIP of the company", required=True)
+    parser.add_argument("email", type=str, help="Company's email address", required=True)
+    parser.add_argument("city", type=str, help="City name", required=True)
+    parser.add_argument("street", type=str, help="Street name", required=True)
+    parser.add_argument("postal_code", type=str, help="City postal code", required=True)
+    parser.add_argument("country", type=str, help="Country name", required=True)
+    parser.add_argument("house_number", type=int, help="House number", required=True)
+    return parser
+def orders_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("date_order", type=str, help="Date in format YYYY-MM-DD", required=True)
+    parser.add_argument("price", type=float, help="Price of the whole order", required=True)
+    parser.add_argument("id_contractor", type=int, help="ID of the contractor who ordered", required=True)
+    parser.add_argument("date_finished", type=str, help="Date in format YYYY-MM-DD", required=False)
+    return parser
+def delivery_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("delivery_date", type=str, help="Date in format YYYY-MM-DD", required=True)
+    parser.add_argument("id_suppier", type=int, help="ID of the supplier who delivered ordered supplies", required=True)
+    parser.add_argument("id_employee", type=int, help="ID of the employee who took the delivery", required=True)
+    return parser
+def material_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("name_material", type=str, help="Name of the material", required=True)
+    parser.add_argument("color", type=str, help="Color of the material", required=False)
+    return parser
+def model_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("name_material", type=str, help="Name of the material", required=True)
+    parser.add_argument("name_model", type=str, help="Name of the model", required=True)
+    parser.add_argument("price_base", type=float, help="Base price of the product of this model", required=True)
+    parser.add_argument("price_promotional", type=float, help="Price of the product of this model on sale", required=False)
+    parser.add_argument("sku", type=str, help="SKU number of the model", required=True)
+    parser.add_argument("available", type=str, help="Y if the product of this model is available, N if not", required=True)
+    parser.add_argument("description", type=str, help="Description of the model", required=False)
+    return parse
+def order_component_parser():
+    parser.add_argument("quantity", type=int, help="Quantity of the ordered product", required=True)
+    parser.add_argument("id_order", type=int, help="ID of the order which consists of given component", required=True)
+    parser.add_argument("id_model", type=int, help="ID of the model which given component represents", required=True)
+    return parser
+def stock_parser():
+    parser.add_argument("quantity", type=int, help="Quantity of material delivered in given delivery", required=True)
+    parser.add_argument("name_material", type=str, help="Name of the material", required=True)
+    parser.add_argument("id_delivery", type=int, help="ID of the delivery in which the stock was registered", required=True)
+    return parser
+
+
+
+
+
 
 
 
